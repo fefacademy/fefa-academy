5dd92eafb486b992f171bc3e177c24bec88520ec
Victor Ndaba <mwangindaba@gmail.com>
2022-06-10 21:41:04 +0300

    📦 feat: initialized tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          main: "#228be6",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
