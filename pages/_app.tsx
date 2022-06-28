f0fb5ead7e22b38e0eea351aa3b74b9357b347b3
Victor Ndaba <mwangindaba@gmail.com>
2022-06-28 15:11:00 +0300

    📦 feat: added mantine provider and color scheme prov

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,47 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+  Paper,
+} from "@mantine/core";
+import Navbar from "../components/Navbar";
+import { useHotkeys, useLocalStorageValue } from "@mantine/hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [colorScheme, setColorScheme] = useLocalStorageValue<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme == "dark" ? "light" : "dark"));
+
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
+  return (
+    <>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          theme={{
+            fontFamily: "inherit",
+            loader: "dots",
+            colorScheme,
+          }}
+        >
+          <Paper className="rounded-none">
+            <div className="font-sans">
+              <Navbar />
+              <Component {...pageProps} />
+            </div>
+          </Paper>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
