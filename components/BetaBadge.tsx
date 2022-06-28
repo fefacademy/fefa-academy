89bd00e119f9266786328caac2fd2742945984fb
Victor Ndaba <mwangindaba@gmail.com>
2022-06-28 15:10:02 +0300

    📦 feat: created the beta badge component

---
+++ components/BetaBadge.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Text } from "@mantine/core";
+
+interface BadgeProps {
+  size?: Size;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+const enum Size {
+  Small = "small",
+  Large = "large",
+}
+
+const BetaBadge: React.FC<BadgeProps> = (props) => {
+  return (
+    <div className="rounded-md bg-yellow-400 text-white px-1 uppercase self-start mx-1">
+      <Text className={`${props.size === "small" ? "text-xs" : "text-xs"} `}>
+        Beta
+      </Text>
+    </div>
+  );
+};
+
+export default BetaBadge;
