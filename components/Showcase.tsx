22e070b1781b84231c42141efadee4d82c59a317
Victor Ndaba <mwangindaba@gmail.com>
2022-07-04 20:10:01 +0300

    🔃 refactor: made the showcase tablet responsive

--- components/Showcase.tsx
+++ components/Showcase.tsx
@@ -6,20 +6,26 @@ const Showcase = () => {
   const dark = colorScheme === "dark";
   const videoSrc = dark ? "/videos/fefa_dark.mp4" : "/videos/fefa_light.mp4";
   return (
-    <div className="w-full space-y-6 md:space-y-0 md:space-x-6 h-full md:h-[calc(100vh-100px)] flex justify-between flex-col md:flex-row p-8 md:px-16 items-center pb-0 md:pb-5">
-      <div className="w-full md:w-1/2 space-y-5 md:space-y-6 ">
-        <Text className="text-5xl leading-[58px] md:text-[3.3rem] font-bold md:leading-[65px]">
+    <div className="w-full space-y-6 lg:space-y-0 lg:space-x-6 h-full lg:h-[calc(100vh-100px)] flex justify-between flex-col lg:flex-row p-8 md:px-10 lg:px-16 items-center pb-0 lg:pb-5">
+      <div className="w-full lg:w-1/2 space-y-5 md:space-y-8 ">
+        <Text className="text-5xl leading-[58px] md:text-[3.3rem] font-bold md:leading-[70px]">
           Providing access to free and high quality content for everyone.
         </Text>
         <Text color={"gray"} className="text-2xl">
           An online academy with a focus on making knowledge available to all,
           for free. Free in every sense of the word.
         </Text>
-        <Button variant="outline" size="lg" className="rounded-full">
-          Start Learning
-        </Button>
+        <div className="center lg:justify-start">
+          <Button
+            variant="outline"
+            size="lg"
+            className="rounded-full self-center lg:self-start"
+          >
+            Start Learning
+          </Button>
+        </div>
       </div>
-      <div className="center w-full md:w-1/2 p-0">
+      <div className="center w-full md:w-11/12 lg:w-1/2 p-0">
         <video
           autoPlay
           loop
