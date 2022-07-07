11ef93930639ad7b1324275523be805d693436e7
Victor Ndaba <mwangindaba@gmail.com>
2022-07-07 22:11:42 +0300

    📦 feat: added showcase for donation page

--- pages/donate/index.tsx
+++ pages/donate/index.tsx
@@ -1,6 +1,13 @@
 import { Card, CardSection, Container, Text } from "@mantine/core";
+import { NextLink } from "@mantine/next";
 import { CheckCircledIcon } from "@modulz/radix-icons";
 import React from "react";
+import {
+  BrandCoinbase,
+  BrandGithub,
+  BrandPatreon,
+  BrandPaypal,
+} from "tabler-icons-react";
 import { useStyles } from "../../lib/shared";
 
 const Item: React.FC<{ children: React.ReactNode }> = (p) => {
@@ -60,7 +67,7 @@ const DonatePage = () => {
       radius: "lg:rounded-tl-2xl lg:rounded-bl-2xl",
       items: [
         "Our sincerest gratitude. Thank you for taking this chance to support us! We really appreciate it.",
-        "The warm feeling you get in your heart after doing something nice. 😁",
+        "The warm feeling you get in your heart after doing something really nice. 😁",
       ],
     },
     {
@@ -85,23 +92,83 @@ const DonatePage = () => {
     },
   ];
 
+  const data = [
+    {
+      title: "Become a Patreon",
+      icon: <BrandPatreon size={30} />,
+      link: "#",
+    },
+    {
+      title: "Sponsor on Github",
+      icon: <BrandGithub size={30} />,
+      link: "#",
+    },
+    {
+      title: "Donate via Paypal",
+      icon: <BrandPaypal size={30} />,
+      link: "#",
+    },
+    {
+      title: "Donate some crypto",
+      icon: <BrandCoinbase size={30} />,
+      link: "#",
+    },
+  ];
+  const platforms = data.map((d, i) => {
+    return (
+      <Card
+        shadow={"md"}
+        component={NextLink}
+        href={d.link}
+        key={i}
+        className="h-full flex flex-row space-x-4 items-center hover:scale-105 transition-all cursor-pointer"
+      >
+        {d.icon}
+        <div className="h-6 opacity-90 border-[1px] rounded-md"></div>
+        <Text className="text-lg">{d.title} </Text>
+      </Card>
+    );
+  });
+
   return (
     <div className={`h-full p-5 lg:p-10`}>
-      <Container className="p-0 max-w-7xl grid grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-0 items-center justify-center lg:p-5">
-        {variants.map((v) => {
-          const { color, items, price, radius, slug } = v;
+      <Container className="p-0 max-w-7xl ">
+        <section className="center flex-col lg:flex-row mb-8 p-5 space-x-5">
+          <div className="w-full lg:w-1/2 space-y-3">
+            <Text className="text-5xl font-medium roboto mb-5">
+              Help us grow
+            </Text>
+            <Text className="text-xl">
+              Fefa is a non-profit organization. It remains alive because of
+              wonderful human beings like you. By donating, or becoming a
+              patreon, you&apos;ll be helping us improve by creating new
+              resources to help people learn new skills. You can donate through
+              various ways and/or platforms.
+            </Text>
+            <Text className="text-xl">
+              If you become a patreon, or subscribe to a recurring donation, the
+              perks associated with this are equivalent to that of a one-time
+              iron-price level donation. All the donation levels are shown below
+            </Text>
+          </div>
+          <div className="w-full lg:w-1/2 space-y-5 lg:px-10">{platforms}</div>
+        </section>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-0 items-center justify-center lg:p-5">
+          {variants.map((v, i) => {
+            const { color, items, price, radius, slug } = v;
 
-          return (
-            <CustomCard
-              key={v.price}
-              price={price}
-              color={color}
-              items={items}
-              radius={radius}
-              slug={slug}
-            />
-          );
-        })}
+            return (
+              <CustomCard
+                key={i}
+                price={price}
+                color={color}
+                items={items}
+                radius={radius}
+                slug={slug}
+              />
+            );
+          })}
+        </div>
       </Container>
     </div>
   );
