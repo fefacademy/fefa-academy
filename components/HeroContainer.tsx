d9e9fc6b2905a131f9c03eca9caca2b6557194bd
Victor Ndaba <mwangindaba@gmail.com>
2022-08-16 16:51:40 +0300

    refactor: renamed showcase to hero container

--- components/HeroContainer.tsx
+++ components/HeroContainer.tsx
@@ -9,13 +9,15 @@ const Showcase = () => {
   return (
     <div className="w-full space-y-6 lg:space-y-0 lg:space-x-6 h-full lg:py-16 flex justify-between flex-col lg:flex-row p-8 md:px-10 lg:px-16 items-center pb-0 lg:pb-5">
       <div className="w-full lg:w-1/2 space-y-5  ">
-        <Text className="text-5xl roboto leading-[58px] md:text-[3.3rem] font-bold md:leading-[70px]">
+        <Text
+          className="text-5xl roboto leading-[58px] md:text-[3.3rem] font-bold md:leading-[70px]"
+          component="h1"
+        >
           Providing access to
           <Text
             component="span"
             variant="gradient"
-            gradient={{ from: "blue", to: "cyan" }}
-            className="bg-gradient-to-r from-teal-500 to-blue-500"
+            gradient={{ from: "teal", to: "blue" }}
             inherit
           >
             {" "}
@@ -25,14 +27,14 @@ const Showcase = () => {
           <Text
             component="span"
             variant="gradient"
-            className="bg-gradient-to-r from-cyan-500 via-teal-400 to-blue-500"
+            gradient={{ from: "teal", to: "indigo" }}
             inherit
           >
             high quality{" "}
           </Text>
           education for all.
         </Text>
-        <Text color={"gray"} className="text-2xl">
+        <Text color={"gray"} className="text-2xl" component="p">
           An online academy with a focus on availing knowledge to everyone, for
           free. Free in every sense of the word.
         </Text>
@@ -41,6 +43,7 @@ const Showcase = () => {
             variant="outline"
             size="lg"
             className="rounded-full self-center lg:self-start btn"
+            aria-label="Start Learning"
           >
             Start Learning
           </Button>
