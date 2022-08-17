c31388ac4c31f2e291e2b9056b0e49496b08e99d
Victor Ndaba <mwangindaba@gmail.com>
2022-08-18 02:35:11 +0300

    refactor: moved old donate page to prev

---
+++ pages/donate.tsx
@@ -0,0 +1,13 @@
+import Page from "components/Page";
+
+export default function Donate() {
+  return (
+    <Page
+      customMeta={{
+        title: "Donate | Fefa Academy",
+        description:
+          "Make a small contribution to help us grow and achieve our mission of providing free and high quality education to all",
+      }}
+    ></Page>
+  );
+}
