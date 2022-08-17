94e1f1d2e1657e62b7f76f74f022a3e9b4f2f0f7
Victor Ndaba <mwangindaba@gmail.com>
2022-08-18 02:27:06 +0300

    feat: updated static site data

--- lib/data.ts
+++ lib/data.ts
@@ -30,6 +30,110 @@ export const NavbarLinks = [
   },
 ];
 
+export const HomepageCTAs = [
+  {
+    link: "/blog",
+    body: "Checkout the latest entries in the Fefa Academy Blog.",
+    title: "Read the latest blog",
+    slug: "blog",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-book" width="60" height="60" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0"></path><path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0"></path><line x1="3" y1="6" x2="3" y2="19"></line><line x1="12" y1="6" x2="12" y2="19"></line><line x1="21" y1="6" x2="21" y2="19"></line></svg>',
+  },
+  {
+    link: "/courses",
+    body: "Find courses and lessons on your favorite topics.",
+    title: "Browse our courses",
+    slug: "courses",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-tv" width="60" height="60" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><rect x="3" y="7" width="18" height="13" rx="2"></rect><polyline points="16 3 12 7 8 3"></polyline></svg>',
+  },
+  {
+    link: "#subscribe_updates",
+    body: "Get notified when new courses are released.",
+    title: "Subscribe to updates",
+    slug: "updates",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-notification" width="60" height="60" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 6h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3"></path><circle cx="17" cy="7" r="3"></circle></svg>',
+  },
+];
+
+export const AcademyBenefits = [
+  {
+    title: "Flexible learning schedule",
+    body: "Only you control when and how you learn. Since the courses are saved for offline use, you can go throught them whenever you prefer, in whatever order you prefer.",
+    slug: "calendar",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-calendar" width="55" height="55" viewBox="0 0 24 24" stroke="teal" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><rect x="4" y="5" width="16" height="16" rx="2"></rect><line x1="16" y1="3" x2="16" y2="7"></line><line x1="8" y1="3" x2="8" y2="7"></line><line x1="4" y1="11" x2="20" y2="11"></line><line x1="11" y1="15" x2="12" y2="15"></line><line x1="12" y1="15" x2="12" y2="18"></line></svg>',
+  },
+  {
+    title: "Active community support",
+    body: " Fefa contains a rich and active community on many platforms. Perhaps too many. Whether you' stuck or looking for advice, we've got you covered!",
+    slug: "people",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-friends" width="55" height="55" viewBox="0 0 24 24" stroke="teal" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="7" cy="5" r="2"></circle><path d="M5 22v-5l-1 -1v-4a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4l-1 1v5"></path><circle cx="17" cy="5" r="2"></circle><path d="M15 22v-4h-2l2 -6a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1l2 6h-2v4"></path></svg>',
+  },
+  {
+    title: "Certificate on completion",
+    body: "Upon course completion, you can request a certificate and depending on the course, you will be required to complete certain tasks/quizes after which you're presented with the course certificate.",
+    slug: "certificate",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-certificate" width="55" height="55" viewBox="0 0 24 24" stroke="teal" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="15" cy="15" r="3"></circle><path d="M13 17.5v4.5l2 -1.5l2 1.5v-4.5"></path><path d="M10 19h-5a2 2 0 0 1 -2 -2v-10c0 -1.1 .9 -2 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -1 1.73"></path><line x1="6" y1="9" x2="18" y2="9"></line><line x1="6" y1="12" x2="9" y2="12"></line><line x1="6" y1="15" x2="8" y2="15"></line></svg>',
+  },
+  {
+    title: "Downloadable courses",
+    body: "Download any course, any time to watch them later without an internet connection. Alternatively, you could save a course to your cloud storage service of choice.",
+    slug: "cloud_download",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-cloud-download" width="55" height="55" viewBox="0 0 24 24" stroke="teal" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M19 18a3.5 3.5 0 0 0 0 -7h-1a5 4.5 0 0 0 -11 -2a4.6 4.4 0 0 0 -2.1 8.4"></path><line x1="12" y1="13" x2="12" y2="22"></line><polyline points="9 19 12 22 15 19"></polyline></svg>',
+  },
+  {
+    title: "Project-based learning",
+    body: "Think project-based learning on steroids. We create multiple real world projects with practical applications to solidify the concepts taught to you in a course/lesson.",
+    slug: "stars",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-stars" width="55" height="55" viewBox="0 0 24 24" stroke="teal" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M17.8 19.817l-2.172 1.138a0.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a0.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a0.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a0.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a0.39 .39 0 0 1 -.567 .411l-2.172 -1.138z"></path><path d="M6.2 19.817l-2.172 1.138a0.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a0.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a0.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a0.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a0.39 .39 0 0 1 -.567 .411l-2.172 -1.138z"></path><path d="M12 9.817l-2.172 1.138a0.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a0.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a0.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a0.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a0.39 .39 0 0 1 -.567 .411l-2.172 -1.138z"></path></svg>',
+  },
+  {
+    title: "Nerdy humour",
+    body: "I can't help myself. I am a nerd and I make nerdy jokes. Even this website is rigged with multiple easter eggs: nerdy references and popular one-liners. Brace yourself.",
+    slug: "happy",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mood-crazy-happy" width="55" height="55" viewBox="0 0 24 24" stroke="teal" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="12" cy="12" r="9"></circle><line x1="7" y1="8.5" x2="10" y2="11.5"></line><path d="M7 11.5l3 -3"></path><line x1="14" y1="8.5" x2="17" y2="11.5"></line><path d="M14 11.5l3 -3"></path><path d="M9.5 15a3.5 3.5 0 0 0 5 0"></path></svg>',
+  },
+  {
+    title: "Learn from the ground-up",
+    body: "Our courses are masterclasses. They assume a novice status and are designed in such a way to take you from whatever level you are in, to an expert in your field.",
+    slug: "stairs_up",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-stairs-up" width="55" height="55" viewBox="0 0 24 24" stroke="teal" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 20h4v-4h4v-4h4v-4h4"></path><path d="M4 11l7 -7v4m-4 -4h4"></path></svg>',
+  },
+  {
+    title: "Problem-centered approach",
+    body: "We don' stack your mind with concepts then find highly contrived use-cases. We take a problem first approach and present new concepts in due time. We cross that bridge when we get there.",
+    slug: "urgent",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-urgent" width="55" height="55" viewBox="0 0 24 24" stroke="teal" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M8 16v-4a4 4 0 0 1 8 0v4"></path><path d="M3 12h1m8 -9v1m8 8h1m-15.4 -6.4l.7 .7m12.1 -.7l-.7 .7"></path><rect x="6" y="16" width="12" height="4" rx="1"></rect></svg>',
+  },
+  {
+    title: "It's totally free",
+    body: "And if that's not enough, remember that it's all absolutely free. It quite literally doesn't get more exciting than that. If not, there's the door. Well, not really, but you get the point.",
+    slug: "cash_off",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-cash-off" width="55" height="55" viewBox="0 0 24 24" stroke="teal" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M13 9h6a2 2 0 0 1 2 2v6m-2 2h-10a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2"></path><path d="M12.582 12.59a2 2 0 0 0 2.83 2.826"></path><path d="M17 9v-2a2 2 0 0 0 -2 -2h-6m-4 0a2 2 0 0 0 -2 2v6a2 2 0 0 0 2 2h2"></path><path d="M3 3l18 18"></path></svg>',
+  },
+];
+
+export const WaysToSupport = [
+  {
+    title: "Donate",
+    body: 'Fefa is a non-profit platform. It&apos;s maintained by the pockets of the owners and our awesome patreons. There are multiple ways that you could help us financially. Consider <a href="/courses" target="_blank" class="text-blue-500 hover:underline cursor-pointer">sponsoring us via github</a> or <a href="/courses" target="_blank" class="text-blue-500 hover:underline cursor-pointer">patreon</a> or <a href="/courses" target="_blank" class="text-blue-500 hover:underline cursor-pointer">buy me a coffee</a>. You could also make a direct <a href="/courses" target="_blank" class="text-blue-500 hover:underline cursor-pointer">donation</a>.',
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="text-green-400" width="60" height="60" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="12" cy="12" r="9"></circle><path d="M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 0 0 0 4h2a2 2 0 0 1 0 4h-2a2 2 0 0 1 -1.8 -1"></path><path d="M12 6v2m0 8v2"></path></svg>',
+  },
+  {
+    title: "Spread the love",
+    body: 'If you feel that the academy has helped you in any way, help us grow by spreading the word, and the love. Share our links on your socials and so on. You can tweet you testimonial at <a class="text-blue-500 hover:underline cursor-pointer" href="/#">@fefaacademy </a> and it will get featured in the testimonials section.',
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="text-orange-400" width="60" height="60" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M19.5 13.572l-7.5 7.428l-2.896 -2.868m-6.117 -8.104a5 5 0 0 1 9.013 -3.022a5 5 0 1 1 7.5 6.572"></path><path d="M3 13h2l2 3l2 -6l1 3h3"></path></svg>',
+  },
+  {
+    title: "Contribute",
+    body: 'Contributions can be made in various ways. Provide feedback to us by taking the surveys and helping us improve. Have an idea, or some constructive criticism ? Don\'t hesitate to let us know, in our public <a class="text-blue-500 hover:underline cursor-pointer" href="/#">discord server. </a> See an issue on the site? <a class="text-blue-500 hover:underline cursor-pointer" href="/#">Fix it on github </a>',
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="text-cyan-400" width="60" height="60" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path><path d="M12 6l-3.293 3.293a1 1 0 0 0 0 1.414l.543 .543c.69 .69 1.81 .69 2.5 0l1 -1a3.182 3.182 0 0 1 4.5 0l2.25 2.25"></path><path d="M12.5 15.5l2 2"></path><path d="M15 13l2 2"></path></svg>',
+  },
+  {
+    title: "Keep seeding",
+    body: "All courses can be downloaded directly, free of charge. To avoid high server costs, the courses are distributed through various means including peer to peer sharing. If you do happen to download any course via torrenting, please do keep seeding.",
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="text-red-400" width="60" height="60" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 12c2 -2.96 0 -7 -1 -8c0 3.038 -1.773 4.741 -3 6c-1.226 1.26 -2 3.24 -2 5a6 6 0 1 0 12 0c0 -1.532 -1.056 -3.94 -2 -5c-1.786 3 -2.791 3 -4 2z"></path></svg>',
+  },
+];
+
 export const HomepageFAQData = [
   [
     "How is Fefa Academy different?",
@@ -69,7 +173,7 @@ export const ManifestoData = [
     body: "Vin Diesel was right: It's all about family. My goals with Fefa include creating a thriving community where people learn new skills, grow professionaly, make new friends, become part of a family! ",
   },
   {
-    title: "III. Because free can be high quality too",
+    title: "III. Because free can be high quality",
     quote:
       "All of us do not have equal talent, but all of us should have an equal opportunity to develop our talent",
     cite: "- John F. Kennedy",
