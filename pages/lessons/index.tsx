de17de2619dce11b7a5e0a277c1fb3318e9e7f76
Victor Ndaba <mwangindaba@gmail.com>
2022-08-18 02:36:55 +0300

    feat: started work on lessons page

--- pages/lessons/index.tsx
+++ pages/lessons/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Page from "components/Page";
 
-const index = () => {
-  return <div>index</div>;
-};
-
-export default index;
+export default function Lessons() {
+  return (
+    <Page
+      customMeta={{
+        title: "Lessons | Fefa Academy",
+        description:
+          "Fast-paced videos/series that assume some knowledge and are designed to tackle difficult to advanced concepts",
+      }}
+    >
+      <h1>Fefa Academy lessons</h1>
+    </Page>
+  );
+}
