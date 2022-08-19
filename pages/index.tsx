f84af9a76923af8de9bbace95bc6c6f4e2b56def
Victor Ndaba <mwangindaba@gmail.com>
2022-08-20 00:06:44 +0300

    refactor: updated page styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,13 @@ import {
   Card,
   CardSection,
   Center,
-  Container,
+  Divider,
   Group,
   Text,
   TextInput,
   useMantineColorScheme,
 } from "@mantine/core";
-import { NextLink } from "@mantine/next";
 import Image from "next/image";
-import Link from "next/link";
 
 import Faq from "components/Faq";
 import Page from "components/Page";
@@ -25,37 +23,17 @@ import {
   ManifestoData,
   WaysToSupport,
 } from "lib/data";
-import { useStyles } from "lib/shared";
+import { useGlobalStyles } from "lib/shared";
 import styles from "styles/Home.module.css";
 
 import Author from "assets/author.jpg";
 import SealImage from "assets/fefa_seal.png";
 import HeartLogo from "assets/heart.png";
 import HomePageHero from "components/sections/HomePageHero";
-
-function GradientText(props: any) {
-  const { from, to, text } = props;
-
-  return (
-    <Text component="span" variant="gradient" gradient={{ from, to }} inherit>
-      {" "}
-      {text}{" "}
-    </Text>
-  );
-}
-
-const InlineLink: React.FC<{ href: string; text: string }> = (props) => {
-  return (
-    <Link href={props.href} passHref>
-      <span className="text-blue-500 hover:underline cursor-pointer">
-        {props.text}{" "}
-      </span>
-    </Link>
-  );
-};
+import Link from "next/link";
 
 export default function Home() {
-  const { classes } = useStyles();
+  const { classes } = useGlobalStyles();
   const { colorScheme } = useMantineColorScheme();
   const iconSize = 60;
   const dark = colorScheme === "dark";
@@ -75,22 +53,21 @@ export default function Home() {
               <Card
                 key={i}
                 shadow={"xs"}
-                className={`flex items-center space-x-5 rounded-lg ${
-                  !dark && "border-2"
-                }`}
+                radius="lg"
+                className={`${classes.borderCard}`}
               >
-                <div dangerouslySetInnerHTML={{ __html: item.icon }}></div>
-                <div className="h-full border-[1px] rounded-md"></div>
-                <div className="space-y-2">
-                  <Text
-                    className="text-lg roboto hover:underline text-blue-500 transition-all"
-                    component={NextLink}
-                    href={item.link}
-                  >
-                    {item.title}
-                  </Text>
-                  <Text>{item.body}</Text>
-                </div>
+                <section className={`flex items-center space-x-5`}>
+                  <div dangerouslySetInnerHTML={{ __html: item.icon }}></div>
+                  <Divider orientation="vertical" size={"sm"} />
+                  <div className="space-y-2">
+                    <span className="cursor-pointer text-lg roboto hover:underline text-blue-500 transition-all">
+                      <Link href={item.link} passHref>
+                        {item.title}
+                      </Link>
+                    </span>
+                    <Text>{item.body}</Text>
+                  </div>
+                </section>
               </Card>
             );
           })}
@@ -99,13 +76,13 @@ export default function Home() {
         {/* Why you'll love Fefa section */}
         <section className="">
           <Center>
-            <Text className="font-semibold hidden md:flex items-center text-4xl roboto mb-3">
+            <h2 className="font-semibold hidden md:flex items-center text-4xl roboto mb-4">
               Why you&apos;ll{" "}
               <span className="mx-3">
                 <Image src={HeartLogo} alt="love"></Image>
               </span>{" "}
               Fefa Academy
-            </Text>
+            </h2>
           </Center>
           <h2 className="md:hidden text-3xl md:text-4xl text-center">
             Why you&apos;ll ❤️ Fefa Academy
@@ -115,13 +92,16 @@ export default function Home() {
               return (
                 <div className="space-y-3" key={idx}>
                   <div dangerouslySetInnerHTML={{ __html: item.icon }}></div>
-                  <Text className="text-2xl font-semibold">{item.title}</Text>
-                  <Text className="text-lg">{item.body}</Text>
+                  <span className="text-2xl font-semibold mt-2">
+                    {item.title}
+                  </span>
+                  <p className="text-lg">{item.body}</p>
                 </div>
               );
             })}
           </div>
         </section>
+
         {/* The Fefa Academy Manifesto */}
         <section
           className={`relative h-full rounded-md overflow-hidden p-5 md:p-7 text-black cursive`}
@@ -134,22 +114,25 @@ export default function Home() {
             objectPosition={"center"}
             className="!z-[0]"
           />
-          <Text className="relative text-2xl md:text-4xl font-semibold text-center !z-[10]">
+          <h3 className="relative text-2xl md:text-4xl font-semibold text-center !z-[10]">
             The Fefa Academy Manifesto
-          </Text>
+          </h3>
           <div className="grid gap-5 md:gap-8 grid-cols-1 md:grid-cols-2 p-3 md:p-8 md:px-14 relative !z-10">
             {ManifestoData.map((d, i) => {
               return (
                 <div key={i} className="space-y-2">
-                  <Text className="text-xl text-center md:text-2xl font-semibold">
+                  <h4 className="text-xl text-center md:text-2xl font-semibold">
                     {d.title}
-                  </Text>
+                  </h4>
                   <Blockquote
                     color={"dark"}
-                    className="text-black p-1"
                     p={"xs"}
                     cite={d.cite}
                     styles={{
+                      root: {
+                        color: "black",
+                        padding: "2px",
+                      },
                       cite: {
                         color: "black",
                         fontWeight: "bold",
@@ -159,7 +142,7 @@ export default function Home() {
                   >
                     {d.quote}
                   </Blockquote>
-                  <Text className="text-lg md:text-xl">{d.body}</Text>
+                  <h5 className="text-lg md:text-xl">{d.body}</h5>
                 </div>
               );
             })}
@@ -180,14 +163,19 @@ export default function Home() {
                 <Card
                   key={i}
                   shadow={"xl"}
-                  className={`${classes.customCard} rounded-xl`}
+                  radius={"lg"}
+                  className={`${classes.customCard}`}
                   p="xl"
                 >
-                  <CardSection className="center flex-col p-2 pt-3">
-                    <div dangerouslySetInnerHTML={{ __html: item.icon }}></div>
-                    <Text className="mt-1 text-2xl font-medium roboto text-center">
-                      {item.title}{" "}
-                    </Text>
+                  <CardSection>
+                    <div className="center flex-col p-2 pt-3">
+                      <div
+                        dangerouslySetInnerHTML={{ __html: item.icon }}
+                      ></div>
+                      <h5 className="mt-1 text-2xl font-medium roboto text-center">
+                        {item.title}{" "}
+                      </h5>
+                    </div>
                   </CardSection>
                   <div dangerouslySetInnerHTML={{ __html: item.body }}></div>
                 </Card>
@@ -204,7 +192,7 @@ export default function Home() {
 
       {/* From the creator section */}
       <section className={`w-full mb-10 ${classes.showcase}`}>
-        <Container className="max-w-7xl p-0 md:p-5 flex flex-col lg:flex-row items-center space-y-8 lg:space-x-8 lg:space-y-0">
+        <div className="mx-auto max-w-7xl p-0 md:p-5 flex flex-col lg:flex-row items-center space-y-8 lg:space-x-8 lg:space-y-0">
           <div className="w-full lg:w-1/2 pt-5 lg:p-10 center flex-col">
             <Image
               alt="victor ndaba"
@@ -213,15 +201,15 @@ export default function Home() {
               width={260}
               height={260}
             />
-            <Button variant="outline" radius={"xl"} size="lg" className="mt-5">
+            <Button variant="outline" radius={"xl"} size="lg" mt={"md"}>
               Get in touch
             </Button>
           </div>
           <div className="w-full lg:w-1/2 p-5 lg:p-0 pt-0">
-            <Text className="text-3xl text-center md:text-left md:text-5xl font-medium roboto mb-5">
+            <h5 className="text-3xl text-center md:text-left md:text-5xl font-medium roboto mb-5">
               From the Founder
-            </Text>
-            <Text className={`text-xl`}>
+            </h5>
+            <Text size={"xl"}>
               Hi, 👋. I&apos;m Victor Ndaba, the founder of this small academy.
               Fefa is like my perfect child: the Ruby to my Matz, the GOT to my
               HBO. I created it because I don&apos;t believe that free and high
@@ -234,15 +222,15 @@ export default function Home() {
               <span className="font-medium">to break the wheel 😉.</span>
             </Text>
           </div>
-        </Container>
+        </div>
       </section>
 
-      <Container className="max-w-7xl p-5 space-y-10">
+      <div className="mx-auto max-w-7xl p-5 space-y-10">
         {/* Join community section */}
         <section className="flex flex-col-reverse lg:flex-row justify-center items-center mb-5">
           <div className="w-full lg:w-1/2 mt-5 lg:mt-0">
             <h2 className="text-4xl mb-5">Join the community...</h2>
-            <Text className="text-xl">
+            <p className="text-xl">
               As stated above, Fefa is more than just another online academy. We
               are a community, a family more like, that will help you learn
               together and grow together. The Fefa Academy community is spread
@@ -260,7 +248,7 @@ export default function Home() {
               on <span className="underline text-blue-400">twitter</span>. As
               for my fellow redditors, I will see you on the{" "}
               <span className="underline text-blue-400">subreddit</span>.
-            </Text>
+            </p>
           </div>
           <div className="h-full w-full relative lg:w-1/2 flex justify-center items-center">
             <Image
@@ -277,10 +265,10 @@ export default function Home() {
           <div
             className={`${classes.customCard} p-5  md:p-8 rounded-md shadow-md`}
           >
-            <Text className="text-3xl roboto font-semibold mb-5">
+            <h5 className="text-3xl roboto font-semibold mb-5">
               Before you go...
-            </Text>
-            <Text className="text-2xl mb-2">Subscribe to our newsletter</Text>
+            </h5>
+            <span className="text-2xl mb-2">Subscribe to our newsletter</span>
             <Text color={"dimmed"}>
               You will never miss important updates, latest news and community
               QA and sessions. Our newsletter is once a week, every Sunday
@@ -288,7 +276,7 @@ export default function Home() {
             <Group spacing={"md"} className="py-2" align="end">
               <TextInput
                 className="w-full md:w-2/3"
-                placeholder="lorenzo.vonmatterhorn@himym.com"
+                placeholder="barryallen@gmail.com"
                 size="lg"
               />
               <Button size="lg" className="!bg-blue-500">
@@ -299,16 +287,16 @@ export default function Home() {
           <div
             className={`${classes.customCard} p-5 md:p-8 rounded-md shadow-md`}
           >
-            <Text className="text-3xl roboto font-semibold mb-5">
+            <h5 className="text-3xl roboto font-semibold mb-5">
               You could also...
-            </Text>
-            <Text className="text-2xl mb-2">Sign the guestbook</Text>
-            <Text color={"dimmed"} className="mb-2">
+            </h5>
+            <span className="text-2xl mb-2">Sign the guestbook</span>
+            <Text color={"dimmed"} mb={"sm"}>
               Leave a message for future visitors. It can be anything from a
               review to an inside-joke to an awesome one-liner.{" "}
               <span className="font-bold">Go ahead, make my day!</span>
             </Text>
-            <Button size="lg" variant="outline" className="rounded-full">
+            <Button size="lg" variant="outline" radius={"xl"}>
               Guestbook
             </Button>
           </div>
@@ -319,7 +307,7 @@ export default function Home() {
           Frequently asked questions
         </h2>
         <Faq data={HomepageFAQData} />
-      </Container>
+      </div>
     </Page>
   );
 }
