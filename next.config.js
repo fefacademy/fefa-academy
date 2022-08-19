6373cd1f6df1b1af27ec29030d9de5f0c81b1d9d
Victor Ndaba <mwangindaba@gmail.com>
2022-08-19 16:24:22 +0300

    build: added experimental features to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
   reactStrictMode: true,
+  images: {
+    domains: [],
+  },
+  experimental: {
+    legacyBrowsers: false,
+    browsersListForSwc: true,
+    images: { allowFutureImage: true },
+  },
 };
 
 module.exports = nextConfig;
