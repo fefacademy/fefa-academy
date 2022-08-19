a5ab23fa36b2622b889bf473033d71d235a5f3d1
Victor Ndaba <mwangindaba@gmail.com>
2022-08-20 00:06:27 +0300

    refactor: added getInitialProps for mantine-next interop

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,9 @@
+import { createGetInitialProps } from "@mantine/next";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class _Document extends Document {
+  static getInitialProps = createGetInitialProps();
+
   render(): JSX.Element {
     return (
       <Html lang="en">
